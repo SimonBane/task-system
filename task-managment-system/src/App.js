5bee6bdd47d41e0b224f8f3030738987b670a21a
simonbane <simeon.banev3@gmail.com>
2018-05-29 00:41:02 +0300

    Started building the Login component and the Route structure.

--- task-managment-system/src/App.js
+++ task-managment-system/src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login/Login';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  state = {
+    isAuthenticated: false,
+    isAdmin: false
+  }
+
   render() {
+    const forceAuthentication = !this.state.isAuthenticated ? <Redirect from="/" to="/login" /> : null;
+
     return (
       <div className="App">
+        {forceAuthentication}
 
+        <Switch>
+          <Route name="Login" path="/login" component={Login} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+      currentUser: state.currentUser
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
---
+++ task-managment-system/src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>Please login before you proceed!</h2>
+
+                </div>
+            </div>
+        );
+    }
+}
