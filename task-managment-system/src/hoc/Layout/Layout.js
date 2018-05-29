4d4671bbb026c0e333e08b64fe5087f886cd643f
simonbane <simeon.banev3@gmail.com>
2018-05-29 21:50:00 +0300

    Fixed up project structure.

---
+++ task-managment-system/src/hoc/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Aux from "../Auxiliary/Auxiliary";
+
+export default class Layout extends Component {
+    render() {
+        return (
+            <Aux>
+
+            </Aux>
+        );
+    }
+}
